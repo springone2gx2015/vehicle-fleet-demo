ac9322a0ac5bc9454a8326bb9e28f3b2ea39864a
Dave Syer <dsyer@pivotal.io>
2015-09-24 17:32:25 +0100

    Reduce zipkin sampling rate so Brave doesn't fall over

--- config-bootstrap/src/main/java/autoconfig/DefaultSamplerAutoConfiguration.java
+++ config-bootstrap/src/main/java/autoconfig/DefaultSamplerAutoConfiguration.java
@@ -16,14 +16,16 @@
 
 package autoconfig;
 
+import java.security.SecureRandom;
+
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.Trace;
 import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.sleuth.zipkin.ZipkinAutoConfiguration;
 import org.springframework.cloud.sleuth.zipkin.ZipkinSpanListener;
 import org.springframework.context.annotation.Bean;
@@ -32,29 +34,45 @@ import org.springframework.context.annotation.Configuration;
 import com.github.kristofa.brave.LoggingSpanCollector;
 import com.github.kristofa.brave.SpanCollector;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Dave Syer
  *
  */
 @Configuration
-@ConditionalOnClass({Trace.class, ZipkinSpanListener.class})
-@AutoConfigureBefore({TraceAutoConfiguration.class, ZipkinAutoConfiguration.class})
+@ConditionalOnClass({ Trace.class, ZipkinSpanListener.class })
+@AutoConfigureBefore({ TraceAutoConfiguration.class, ZipkinAutoConfiguration.class })
+@ConfigurationProperties("fleet.zipkin")
 public class DefaultSamplerAutoConfiguration {
+
+	SecureRandom random = new SecureRandom();
+
+	@Getter
+	@Setter
+	double sampleRate = 0.1;
+
+	@Getter
+	@Setter
+	private boolean enabled;
 
 	@Bean
 	@ConditionalOnMissingBean(Sampler.class)
-	public AlwaysSampler defaultSampler() {
-		return new AlwaysSampler();
+	public Sampler<?> defaultSampler() {
+		return (Object context) -> {
+			return this.random.nextFloat() < this.sampleRate;
+		};
 	}
-
 
 	@Configuration
 	@ConditionalOnClass(ZipkinSpanListener.class)
 	protected static class ZipkinCollectorAutoConfiguration {
 
-		// Use this for debugging (or if there is no Zipkin collector running on port 9410)
+		// Use this for debugging (or if there is no Zipkin collector running on port
+		// 9410)
 		@Bean
-		@ConditionalOnProperty(value="fleet.zipkin.enabled", havingValue="false", matchIfMissing=true)
+		@ConditionalOnProperty(value = "fleet.zipkin.enabled", havingValue = "false", matchIfMissing = true)
 		public SpanCollector spanCollector() {
 			return new LoggingSpanCollector();
 		}
